9d45015e6e15187c4762573c3afde443f27eb4a5
David <daviiideveloper@gmail.com>
2022-08-18 10:56:38 +0200

    Update targets

--- install.py
+++ install.py
@@ -15,7 +15,7 @@ class Install :
         self.loader_anim = loader_01
         self.loader_rs = [loader_01, loader_02, loader_03, loader_04, loader_05, loader_06, loader_07, loader_08]
         self.model = ''
-        self.allow_devices = ["j5nlte", "j5lte", "j5ltechn", "j5xnlte", "j53gxx"]
+        self.allow_devices = ["j5nlte", "j5lte", "j5ltechn", "j5xnlte", "j53gxx", "j53g"]
         self.sideload_counter = 0
         self.reboot_system = False
         self.reboot_counter = 500
